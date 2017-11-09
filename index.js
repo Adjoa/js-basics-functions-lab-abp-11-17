7c0e145bf0226c56687bf185435baf95d4835c25
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-09 17:47:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  if (pickup > 42) {
+    let blocks = pickup - 42;
+    return blocks;
+  } else if (pickup < 42) {
+    let blocks = 42 - pickup
+    return blocks;
+  }
+}
+
+function distanceFromHqInFeet(pickup) {
+  let blocks = distanceFromHqInBlocks(pickup);
+  let feet = blocks * 264;
+  return feet;
+}
+
+function distanceTravelledInFeet(start, end) {
+
+}
+
+function calculatesFarePrice() {
+
+}

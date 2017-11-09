62b87796a51d4a2ab3d02aff3b345d3c98e0745d
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-09 18:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function distanceTravelledInFeet(startingBlock, endingBlock) {
   return feet;
 }
 
-/*
+
 function calculatesFarePrice(startingBlock, endingBlock) {
   let feet = distanceTravelledInFeet(startingBlock, endingBlock);
   let fare;
@@ -41,4 +41,4 @@ function calculatesFarePrice(startingBlock, endingBlock) {
     }
   }
 }
-*/
+

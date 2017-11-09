df8ee05a335ee1fe1855b60d3324090b18577b04
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-09 18:14:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(pickupLocation) {
-  let blocks = Math.abs(pickupLocation - 42);
-  return blocks;
+  if (pickupLocation > 42) {
+    let blocks = pickupLocation - 42;
+    return blocks;
+  } else if (pickupLocation < 42) {
+    let blocks = 42 - pickupLocation
+    return blocks;
+  }
 }
 
 function distanceFromHqInFeet(pickupLocation) {

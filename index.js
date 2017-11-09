ab7f4708856d080d2bb87f1d149c9fb22d40737d
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-09 18:07:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,13 +25,9 @@ function distanceTravelledInFeet(startingBlock, endingBlock) {
 function calculatesFarePrice(startingBlock, endingBlock) {
   let feet = distanceTravelledInFeet(startingBlock, endingBlock);
   let fare;
-
-  if (feet <= 400) {
-    return fare = 0;
-  }
 }
 
-  
+
 /*    case (feet >= 400 && feet <= 2000) {
       return fare = feet * 0.02;
       break;
